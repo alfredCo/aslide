10ebde82b8f8c592c957b79fd315a485cf2aed04
alfred <chenjhfrontend@163.com>
2015-01-13 18:57:04 +0800

    second

---
+++ html-template/js/common.js
@@ -0,0 +1,18 @@
+// JavaScript Document
+
+if(jQuery)(function($){
+   $.extend($.easing,{
+        easeOutCubic: function (x, t, b, c, d) {
+            return c * ((t = t / d - 1) * t * t + 1) + b;
+        },
+        easeOutExpo: function (x, t, b, c, d) {
+            return (t == d) ? b + c : c * (-Math.pow(2, -10 * t / d) + 1) + b;
+        }
+    });
+
+	$(document).ready(function(){
+		
+	
+})(jQuery);
+
+
